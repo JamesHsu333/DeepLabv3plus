975f074dc247006eb3fd417fe1a632388ed81886
James Hsu <jamescow06@gmail.com>
2021-06-21 06:20:52 +0000

    feat: Add writer, epoch for __main__

--- evaluate.py
+++ evaluate.py
@@ -15,17 +15,18 @@ from model.deeplab import *
 from model.sync_batchnorm.replicate import patch_replication_callback
 from utils.loss import loss_fns
 from utils.metrics import Evaluator
+from utils.summary import TensorboardSummary
 import utils.utils as utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='experiments',
-                    help="Directory containing params.json")
+        help="Directory containing params.json")
 parser.add_argument('--model_type', default='deeplab',
-                    help="Type of deeplab")
+        help="Type of deeplab")
 parser.add_argument('--num_classes', default=21,
-                    help="Numbers of classes")
+        help="Numbers of classes")
 parser.add_argument('--restore_file', default='best', help="name of the file in --model_dir \
-                     containing weights to load")
+        containing weights to load")
 
 def evaluate(model, dataloader, loss_fns, evaluator, writer, epoch, params):
     model.eval()
@@ -53,7 +54,7 @@ def evaluate(model, dataloader, loss_fns, evaluator, writer, epoch, params):
             evaluator.add_batch(labels_batch, output_batch)
             # update the average loss
             loss_avg.update(loss.item())
-                
+
             t.set_postfix(loss='{:05.3f}'.format(loss_avg()))
             t.update()
     writer.add_scalar('val/mean_loss_epoch', loss_avg(), epoch)
@@ -63,7 +64,7 @@ def evaluate(model, dataloader, loss_fns, evaluator, writer, epoch, params):
     writer.add_scalar('val/fwIoU', evaluator.Frequency_Weighted_Intersection_over_Union(), epoch)
     metrics_mean = {'mIOU': evaluator.Mean_Intersection_over_Union(), 'loss': loss_avg()}
     metrics_string = " ; ".join("{}: {:05.3f}".format(k, v)
-                                for k, v in metrics_mean.items())
+            for k, v in metrics_mean.items())
     logging.info("- Eval metrics : " + metrics_string)
     return metrics_mean
 
@@ -72,7 +73,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     json_path = os.path.join(args.model_dir, 'params.json')
     assert os.path.isfile(
-        json_path), "No json configuration file found at {}".format(json_path)
+            json_path), "No json configuration file found at {}".format(json_path)
     params = utils.Params(json_path)
 
     params.cuda = torch.cuda.is_available()
@@ -94,10 +95,10 @@ if __name__ == '__main__':
     logging.info("-done")
 
     model = DeepLab(num_classes=args.num_classes,
-                    backbone="resnet",
-                    output_stride=16,
-                    sync_bn=False,
-                    freeze_bn=False)
+            backbone="resnet",
+            output_stride=16,
+            sync_bn=False,
+            freeze_bn=False)
 
     if params.cuda:
         model = nn.DataParallel(model, device_ids=[0])
@@ -110,6 +111,10 @@ if __name__ == '__main__':
 
     evaluator = Evaluator(20+1)
 
+    # Define Tensorboard Summary
+    summary = TensorboardSummary(args.model_dir)
+    writer = summary.create_summary()
+
     logging.info("Starting evaluation")
 
     # Reload weights from the saved file
@@ -117,8 +122,8 @@ if __name__ == '__main__':
         args.model_dir, args.model_type + '_' + args.restore_file + '.pth.tar'), model)
 
     # Evaluate
-    test_metrics = evaluate(model, test_dl, loss_fns, evaluator, params)
+    test_metrics = evaluate(model, test_dl, loss_fns, evaluator, writer, 1, params)
     save_path = os.path.join(
-        args.model_dir, "metrics_test_{}.json".format(args.restore_file))
+            args.model_dir, "metrics_test_{}.json".format(args.restore_file))
     utils.save_dict_to_json(test_metrics, save_path)
     logging.info("- done.")
